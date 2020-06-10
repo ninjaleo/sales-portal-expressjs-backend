049eb233fdfdeb1f69f22d8f1da9da0772b61f77
ninjaleo <63445543+ninjaleo@users.noreply.github.com>
2020-06-10 21:13:58 +0100

    Update app.js
    Applied fix for CORS issue

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.post('/login', routes)
 app.post('/signup', routes)
 
